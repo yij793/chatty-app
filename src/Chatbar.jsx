7b846db8468c69312aa8f7116862a52066a86dee
YIYAO <jianyy1990@hotmail.com>
2019-05-07 16:48:25 +0000

    add new propsto send value to app jsx

--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -2,12 +2,23 @@ import React, { Component } from 'react'
 
 class Chatbar extends Component {
   render() {
+    const addtext=(e)=>{
+      console.log()
+      if(e.key==='Enter'){
+        let contents=e.target.value
+        if(contents===null){e.preventDefalut()}else{ this.props.newText(contents)}
+       }
+      
+    }
+    const changeName=(e)=>{
+      let newValue=e.target.value
+      this.props.changeStateName(newValue)
+    }
     return (
-     
-       <footer className="chatbar">
-       <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-</footer>
+      <footer className="chatbar">
+         <input className="chatbar-username" placeholder="Your Name (Optional)" name='username' value={this.props.value}  onChange={changeName}/>
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" name='content'  onKeyPress={addtext}/>
+      </footer>
     )
   }
 }
