68e1d745ad64d03b3fc58d2909b2215f571b2a1d
YIYAO <jianyy1990@hotmail.com>
2019-05-08 21:39:58 +0000

    add blur and focus props

--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -6,18 +6,19 @@ class Chatbar extends Component {
       console.log()
       if(e.key==='Enter'){
         let contents=e.target.value
-        if(contents===null){e.preventDefalut()}else{ this.props.newText(contents)}
-       }
+        if(contents!==null || contents!=='' )
+        this.props.newText(contents)}
+       
       
     }
     const changeName=(e)=>{
-      let newValue=e.target.value
+      const newValue=e.target.value
       this.props.changeStateName(newValue)
     }
     return (
       <footer className="chatbar">
-         <input className="chatbar-username" placeholder="Your Name (Optional)" name='username' value={this.props.value}  onChange={changeName}/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" name='content'  onKeyPress={addtext}/>
+         <input className="chatbar-username" placeholder="Your Name (Optional)"  value={this.props.value}  onChange={changeName} onBlur={this.props.blur} onFocus={this.props.focus}/>
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER"   onKeyPress={addtext}/>
       </footer>
     )
   }
