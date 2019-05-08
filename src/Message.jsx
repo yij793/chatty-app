a09cc3981524c6ce77541dc22f30bf379b7cd58a
YIYAO <jianyy1990@hotmail.com>
2019-05-08 21:40:23 +0000

    add new type of message

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,16 +3,18 @@ class Message extends Component {
   render() {
     if(this.props.type==='incomingNotification'){
       return (<div className="message system">
-      Anonymous1 changed their name to nomnom.
+    {this.props.note}
          </div>)
-    }
-    return(
+    }else
+    {return(
+      <div>
       <div className="message">
       <span className="message-username" >{this.props.name}</span>
       <span className="message-content" >{this.props.content}</span>
       </div>
-      
-    )
+
+</div>
+    )}
   
   }
 }
