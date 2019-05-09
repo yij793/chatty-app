549ffe0ff80c121e8e42d451be897280cbf193f6
YIYAO <jianyy1990@hotmail.com>
2019-05-09 13:23:29 +0000

    add switch function in serves side to check incoming message

--- chattySevers/chatty-server.js
+++ chattySevers/chatty-server.js
@@ -3,6 +3,8 @@ const SocketServer = require('ws').Server;
 const WebSocket=require('ws')
 // Set the port to 3001
 const PORT = 3001;
+const uuidv1 = require('uuid/v1');
+const uuidv3 = require('uuid/v3');
 
 // Create a new express server
 const server = express()
@@ -18,17 +20,41 @@ const wss = new SocketServer({ server });
 wss.on('connection', function connection(ws) {
   wss.clients.forEach(function each(client) {
       client.send(wss.clients.size);
-    
+  })
   ws.on('message', function incoming(data) {
+    let jsonData=JSON.parse(data)
+    switch(jsonData.type){
+      case "incomingMessage":
+        newData={
+          id:uuidv1(),
+          username:jsonData.username,
+          content:jsonData.content,
+          type:'incomingMessage'
+        }
+        break;
+      case "incomingNotification":
+      newData={
+        id:uuidv1(),
+        note:jsonData.note,
+        type:'incomingNotification'
+      }
+        break;
+      default:
+        // show an error in the console if the message type is unknown
+        throw new Error("Unknown event type " + data.type);
+    
+  }
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
+        client.send(JSON.stringify(newData));
       }
     })
-  });
+  })
   ws.on('close', () => {
     console.log('Client disconnected')
     wss.clients.forEach(function each(client) {
       client.send(wss.clients.size);
   })
 })});
+
+
