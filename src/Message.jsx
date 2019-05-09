84bdd735c353958d91f3c4e8cfe67de9dc1e7daf
YIYAO <jianyy1990@hotmail.com>
2019-05-09 19:21:41 +0000

    change the style by control the state

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 class Message extends Component {
   render() {
+    const colors={color:this.props.color}
     if(this.props.type==='incomingNotification'){
-      console.log('this is a incomingNotification')
-      console.log(this.props.note)
       return (<div className="message system">
     {this.props.note}
          </div>)
     }else
-    {return(
+
+    {
+      return(
       <div>
       <div className="message">
-      <span className="message-username" >{this.props.name}</span>
+      <span className={`message-username`} style={colors} >{this.props.name}</span>
       <span className="message-content" >{this.props.content}</span>
       </div>
 
