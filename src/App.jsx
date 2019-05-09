162c1e41590f56147043ceec7daedb0ff1f551c7
YIYAO <jianyy1990@hotmail.com>
2019-05-09 13:22:00 +0000

    fixing the switch function

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const uuidv1 = require('uuid/v1');
 class App extends Component {
   constructor(props){
     super(props)
-    this.state={messagesDB: [],username:'Anonymous',preverseName:'Anonymous',user:0}
+    this.state={messagesDB: [],username:'Anonymous',preverseName:'Anonymous',user:0,postmessages:[]}
      this.ws = new WebSocket('ws://localhost:3001')
   }
   
@@ -18,14 +18,32 @@ class App extends Component {
     }
     this.ws.onmessage = (event)=> {
       let newData=JSON.parse(event.data);
-      console.log(typeof newData)
      if(typeof newData==='number'){
        this.setState({user:newData})
+     }else{
+      switch(newData.type) {
+        case "incomingMessage":
+        let newstate1=this.state.messagesDB
+        newstate1.push(newData)
+        this.setState({messagesDB:newstate1})
+          break;
+        case "incomingNotification":
+        let newstate2=this.state.messagesDB
+        newstate2.push(newData)
+        console.log('notes is ',newData)
+        this.setState({messagesDB:newstate2})
+          break;
+        default:
+          // show an error in the console if the message type is unknown
+          throw new Error("Unknown event type " + newData.type);
      }
-      
-     let newstate=this.state.messagesDB
-        newstate.push(newData)
-        this.setState({messagesDB:newstate})
+    
+     
+
+    }
+     
+ 
+     
       
     }
     }
@@ -39,7 +57,6 @@ class App extends Component {
     let username=this.state.username;
     if(username===null || username===''){username='Anonymous'}
     let newtext={
-      id:uuidv1(),
       type:'incomingMessage',
       content:text,
       username:username
@@ -58,7 +75,6 @@ class App extends Component {
     let notifction={
       type:'incomingNotification',
       note:notes,
-      id:uuidv1()
     }
     if(this.state.preverseName!==this.state.username)
     this.ws.send(JSON.stringify(notifction))
@@ -74,7 +90,7 @@ class App extends Component {
         <a href="/" className="navbar-brand">Chatty</a>
         <span className='displayOnline'>{this.state.user} online </span>
       </nav>
-      <MessageList infors={this.state.messagesDB}/>
+      <MessageList infors={this.state.messagesDB} notifction={this.state.postmessages}/>
       <Chatbar newText={this.addText} value={this.state.username} changeStateName={this.changeName} blur={this.sendNote} focus={this.getPreverseName}/>
     </div>
       
