d066fc7fe41203738c191f495fa5730e2af91aca
YIYAO <jianyy1990@hotmail.com>
2019-05-10 17:23:29 +0000

    build pattern for App container to manage states

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Chatbar from './Chatbar.jsx';
 import MessageList from './MessageList.jsx';
-import messagesDB from './messagesDB.json';
 
 class App extends Component {
   constructor(props){
@@ -40,48 +39,60 @@ class App extends Component {
         this.setState({messagesDB:newstate3})
           break;
         default:
+        console.log('error data is',newData)
           // show an error in the console if the message type is unknown
           throw new Error("Unknown event type " + newData.type);
     }     
     }
     }
 
-  changeName=(newName)=>{
+  changeName=(e)=>{
+    const newName=e.target.value
     this.setState({username : newName}) 
   }
-  addText=(text)=>{
-    let username=this.state.username;
-    if(username===null || username===''){username='Anonymous'}
-    let reg=/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g;
-    if(reg.test(text)){
-      let url=text.match(reg);
-      let newMesg=text.replace(reg,'');
-      let newMesgs={
-        type:'withPics',
-        content:newMesg,
-        url:url,
-        username:username,
-        color:this.state.currentColor
+  addText=(e)=>{
+    if(e.key==='Enter'){
+      let text=e.target.value
+      if(text.trim()!==null && text.trim()!=='' ){
+        e.target.value=''
+        let username=this.state.username;
+        if(username===null || username===''){username='Anonymous'}
+        let reg=/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g;
+        ///test if infor contains image
+        if(reg.test(text)){
+          let url=text.match(reg);
+          let newMesg=text.replace(reg,'');
+          let newMesgs={
+            type:'withPics',
+            content:newMesg,
+            url:url,
+            username:username,
+            color:this.state.currentColor
+          }
+          this.ws.send(JSON.stringify(newMesgs))
+    
+        }else{
+          let newtext={
+          type:'incomingMessage',
+          content:text,
+          username:username,
+          color:this.state.currentColor
+        }
+        this.ws.send(JSON.stringify(newtext))
       }
-      this.ws.send(JSON.stringify(newMesgs))
-
-    }else{
-      console.log('this should not')
-      let newtext={
-      type:'incomingMessage',
-      content:text,
-      username:username,
-      color:this.state.currentColor
-    }
-    this.ws.send(JSON.stringify(newtext))
-  }
-    
+        
+      }else{
+        console.log('display the error')
+      }
+      }
     
   }
+  ///get PreverseName when focus
   getPreverseName=(e)=>{
     let name=e.target.value
     this.setState({preverseName:name})
   }
+  ///send noticy to severs when blur
   sendNote=(e)=>{
     let name=e.target.value;
     if(name===null || name===''){name='Anonymous'}
@@ -93,30 +104,56 @@ class App extends Component {
     if(this.state.preverseName!==this.state.username && this.state.preverseName!=='')
     this.ws.send(JSON.stringify(notifction))
   }
-
-
-
   render() {
     return (
-     
-    <div>
-      <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty</a>
-        <span className='displayOnline'>{this.state.user} online </span>
-      </nav>
-      <MessageList 
-      infors={this.state.messagesDB}/>
-      <Chatbar 
-      newText={this.addText} 
-      value={this.state.username} 
-      changeStateName={this.changeName} 
-      blur={this.sendNote} 
-      focus={this.getPreverseName}/>
+      <AppPrenster
+        addText={this.addText}
+        messagesDB={this.state.messagesDB}
+        changeName={this.changeName}
+        getPreverseName={this.getPreverseName}
+        username={this.state.username}
+        user={this.state.user}
+        sendNote={this.sendNote}
+
+      />
+    // <div>
+    //   <nav className="navbar">
+    //     <a href="/" className="navbar-brand">Chatty</a>
+    //     <span className='displayOnline'>{this.state.user} online </span>
+    //   </nav>
+    //   <MessageList 
+    //   infors={this.state.messagesDB}/>
+    //   <Chatbar 
+    //   newText={this.addText} 
+    //   value={this.state.username} 
+    //   changeStateName={this.changeName} 
+    //   blur={this.sendNote} 
+    //   focus={this.getPreverseName}/>
       
-    </div>
+    // </div>
       
     );
   }
 }
 export default App;
 
+//// APP Pattern
+const AppPrenster=props=>{
+  return(
+  <div>
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+        <span className='displayOnline'>{props.user} online </span>
+      </nav>
+      <MessageList 
+      infors={props.messagesDB}/>
+      <Chatbar 
+      newText={props.addText} 
+      value={props.username} 
+      changeStateName={props.changeName} 
+      blur={props.sendNote} 
+      focus={props.getPreverseName}/>
+      
+    </div>
+  )
+}
