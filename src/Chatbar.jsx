b34b7964762979cbd047d24840f02142dc57293f
YIYAO <jianyy1990@hotmail.com>
2019-05-10 17:24:19 +0000

    using function compoent instead

--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -1,31 +1,38 @@
 import React, { Component } from 'react'
 
-class Chatbar extends Component {
-  render() {
-    const addtext=(e)=>{
-      if(e.key==='Enter'){
-        let contents=e.target.value
-        if(contents.trim()!==null && contents.trim()!=='' ){
-          this.props.newText(contents)
-          e.target.value=''
-        }
-        }else{
-          console.log('NO!!!!')
-        }
-       
-      
-    }
-    const changeName=(e)=>{
-      const newValue=e.target.value
-      this.props.changeStateName(newValue)
-    }
-    return (
-      <footer className="chatbar">
-         <input className="chatbar-username" placeholder="Your Name (Optional)"  value={this.props.value}  onChange={changeName} onBlur={this.props.blur} onFocus={this.props.focus}/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER"   onKeyPress={addtext}/>
-      </footer>
-    )
-  }
-}
+// class Chatbar extends Component {
+//   render() {
+//     return (
+//       <footer className="chatbar">
+//          <input className="chatbar-username" placeholder="Your Name (Optional)"  
+//          value={this.props.value}  
+//          onChange={this.props.changeStateName} 
+//          onBlur={this.props.blur} 
+//          onFocus={this.props.focus}/>
+//         <input className="chatbar-message" placeholder="Type a message and hit ENTER"   
+//         onKeyPress={this.props.newText}/>
+//         <span style={{color:'red',display:'none'}}>you cannot send empty</span>
+//       </footer>
+//     )
+//   }
+// }
 
+//////Function Compoent//////
+function Chatbar(props){
+  const {value,blur,focus,newText,changeStateName}=props
+  return (
+    <footer className="chatbar">
+       <input className="chatbar-username" placeholder="Your Name (Optional)"  
+       value={value}  
+       onChange={changeStateName} 
+       onBlur={blur} 
+       onFocus={focus}/>
+      <input className="chatbar-message" placeholder="Type a message and hit ENTER"   
+      onKeyPress={newText}/>
+      <span style={{color:'red',display:'none'}}>you cannot send empty</span>
+    </footer>
+  )
+    
+}
+          
 export default Chatbar
