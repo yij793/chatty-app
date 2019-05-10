412b790a5b8611eb4acbf6f814f76f1d56c0bb2e
YIYAO <jianyy1990@hotmail.com>
2019-05-10 15:19:03 +0000

    change to function commpoent

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
 import Message from './Message.jsx'
 import { generateRandomId } from './idgenerator.jsx'
-class MessageList extends Component {
-  render() {
-    let message=this.props.infors;
-    console.log(message)
-    let totalMessage=message.map(item=>
-        <Message name={item.username} content={item.content} key={generateRandomId()} type={item.type} note={item.note} color={item.color} url={item.url}/>
-    )
-    return (
-        <div>
-            <main className="messages"> 
-                    {totalMessage}
-            </main>
-        </div>
+// class MessageList extends Component {
+//   render() {
+//     let message=this.props.infors;
+//     let totalMessage=message.map(item=>
+//         <Message name={item.username} content={item.content} key={generateRandomId()} type={item.type} note={item.note} color={item.color} url={item.url}/>
+//     )
+//     return (
+//         <div>
+//             <main className="messages"> 
+//                     {totalMessage}
+//             </main>
+//         </div>
    
+//     )
+//   }
+// }
+////Function Compoent/////
+function MessageList(props){
+    const {infors}=props
+    const totalMessage=infors.map(item=>
+        <Message 
+        name={item.username} 
+        content={item.content} 
+        key={generateRandomId()} 
+        type={item.type} 
+        note={item.note} 
+        color={item.color} 
+        url={item.url}/>
     )
-  }
+    return ( 
+    <div>
+        <main className="messages"> 
+                {totalMessage}
+        </main>
+    </div>)
 }
 export default MessageList
 
