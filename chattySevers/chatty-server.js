e90f0cdc6d02219faa407026871b847b8e73d851
YIYAO <jianyy1990@hotmail.com>
2019-05-13 01:23:49 +0000

    using destructur to get data

--- chattySevers/chatty-server.js
+++ chattySevers/chatty-server.js
@@ -10,13 +10,13 @@ const server = express()
 .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 //////
 const wss = new SocketServer({ server });
-const color=['rgb(53, 101, 0)','rgb(170, 137, 57)','rgb(170, 63, 57)','rgb(65, 48, 117)','rgb(39, 86, 107)']
+const color=['rgb(53, 101, 0)','rgb(170, 137, 57)','rgb(170, 63, 57)','rgb(65, 48, 117)','rgb(39, 86, 107)','rgb(88, 42, 114']
 /////when start a connect
 wss.on('connection', function connection(ws) {
   console.log('Client Connected')
   const colors={
     type:'color',
-    color:color[Math.floor(Math.random()*5)]
+    color:color[Math.floor(Math.random()*6)]
   }
   ws.send(JSON.stringify(colors));
 
@@ -32,37 +32,37 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {  
     const jsonData=JSON.parse(data)/////parse json data
     /////divide datatype
-    switch(jsonData.type){
+    const {type,username,content,color,note,url}=jsonData
+    switch(type){
       case "incomingMessage":
         newData={
           id:uuidv1(),
-          username:jsonData.username,
-          content:jsonData.content,
+          username:username,
+          content:content,
           type:'incomingMessage',
-          color:jsonData.color
+          color:color
         }
         break;
       case "incomingNotification":
       newData={
         id:uuidv1(),
-        note:jsonData.note,
+        note:note,
         type:'incomingNotification'
       }
       break;
       case "withPics":
       newData={
         id:uuidv1(),
-        username:jsonData.username,
-        content:jsonData.content,
+        username:username,
+        content:content,
         type:'withPic',
-        color:jsonData.color,
-        url:jsonData.url
+        color:color,
+        url:url
       }
       break;
       default:
-      console.log(data)
         // show an error in the console if the message type is unknown
-        throw new Error("Unknown event type " + data.type);
+        throw new Error("Unknown event type " + type);
     
   }
   ////////Send back to Client
